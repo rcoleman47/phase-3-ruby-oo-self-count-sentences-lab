8f62e987e1bd96149f4e7d18950e75ff09044f8b
ryanadams <rcoleman.adams@gmail.com>
2022-05-17 21:39:29 -0500

    commit tree a52feaef79dc68e6c272882f4372614006f2a99e

--- lib/count_sentences.rb
+++ lib/count_sentences.rb
@@ -3,18 +3,22 @@ require 'pry'
 class String
 
   def sentence?
-    
+    self.end_with?(".")
   end
 
   def question?
-
+    self.end_with?("?")
   end
 
   def exclamation?
-
+    self.end_with?("!")
   end
 
   def count_sentences
-
+    self.split(/[!?.]/).reject(&:empty?).map {|c| c.strip}.length
+    
   end
 end
+
+
+"".count_sentences
